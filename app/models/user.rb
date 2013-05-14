0793523c6869de45e62faf5093119f2f962dc3ad
Ke+Angelo <Ke+Angelo@appacademy.io>
2013-05-14 13:42:52 -0400

    signin and signout working

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,19 @@ class User < ActiveRecord::Base
   attr_accessible :name, :password, :password_confirmation, :emails_attributes
   has_secure_password
 
+  before_save :create_remember_token
+
   has_many :emails, dependent: :destroy
   accepts_nested_attributes_for :emails
   #has_many :stories, foreign_key: :author_id
   #has_many :comments
 
   validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
+
+  private
+
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Hnclone::Application.routes.draw do
+  root to: 'users#new'
+
   resources :users
+  resource :sessions, only: [:create, :new, :destroy]
 
   match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy'
 end
