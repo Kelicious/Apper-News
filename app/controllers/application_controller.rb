47d2d2f2e1235f5aeb013f9333f69f923d091ddc
Ke Li <like@umich.edu>
2013-07-21 23:26:07 -0400

    Skip store location if logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,15 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   protect_from_forgery
 
-  #
   def handle_unverified_request
     sign_out
     super
   end
 
   def signed_in_user
-    store_location
-    redirect_to signin_url, notice: "Please sign in" unless signed_in?
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in"
+    end
   end
 end
